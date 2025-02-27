909f724a380fed1c5ba7ffff9219078fe38c2d19
阿逼 <bbnocode@outlook.com>
2025-02-27 17:18:49 +0800

    feat: home page

---
+++ apps/web/app/(documentation)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+export default function Home() {
+	return (
+		<div className="min-w-screen min-h-screen bg-white relative">
+			<div className="cloud-1" />
+			<div className="cloud-2" />
+			<div className="cloud-3" />
+			<div className="cloud-4" />
+		</div>
+	);
+}
